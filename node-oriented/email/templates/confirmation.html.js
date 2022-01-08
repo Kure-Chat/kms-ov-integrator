ebe839f705a7bc8ebfa54a33eb5f5d34e2c6cd29
zlatnaspirala <zlatnaspirala@gmail.com>
2022-01-09 00:42:06 +0100

    wip

--- node-oriented/email/templates/confirmation.html.js
+++ node-oriented/email/templates/confirmation.html.js
@@ -12,12 +12,12 @@ module.exports = {
     return "<div \
       style='background:#000000;color:#0ea8d8;height: 550px;font-size: large;  \
       padding: 20px;' > \
-      <h2> Welcome to Crypto Game War dear " + userName + ". </h2><br/> \
+      <h2> Welcome to Kure Chat dear " + userName + ". </h2><br/> \
       <p>You will need to confirm you email address: <br/> \
       Copy/Paste in your SignUp form.</p> <br/> \
       CODE : <p>" + token + "</p>  <br/><br/><br/> \
       <img style='width:80px;' src='https://github.com/zlatnaspirala/visual-ts-game-engine/blob/master/logo.png?raw=true' /> \
-      Powered by <small>Visual ts game engine.</small> \
+      Powered by <small>maximumroulette.com</small> \
       </div>";
   }
 
